36d91103aa86c6a4b94ff90a5612f6550b3f181d
Rainer Brockerhoff <rainer@brockerhoff.net>
2014-06-05 18:27:49 -0300

    Initial commit with empty project.

---
+++ SwiftChecker/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SwiftChecker
+//
+//  Created by Rainer Brockerhoff on 5/6/14.
+//  Copyright (c) 2014 Rainer Brockerhoff. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification?) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SwiftChecker/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftChecker
+//
+//  Created by Rainer Brockerhoff on 5/6/14.
+//  Copyright (c) 2014 Rainer Brockerhoff. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
